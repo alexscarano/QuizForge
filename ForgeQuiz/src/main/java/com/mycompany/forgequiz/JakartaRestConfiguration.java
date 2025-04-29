5d2502c47c99d44a683a1ac6289edd911cd8138c
Normilando <86866114+Normilando@users.noreply.github.com>
2025-04-29 16:35:05 -0300

    Adicionando Front no projeto
    Modificando os arquivos.

---
+++ ForgeQuiz/src/main/java/com/mycompany/forgequiz/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.forgequiz;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# QuizForge
-Com essa aplicação web o estudante podera criar provas personalizadas com suas necessidades de estudos, utilizando a IA.
-
-## Tipos de provas que poderam ser geradas
-- Provas de multipla escolha
