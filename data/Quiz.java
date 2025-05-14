780959db16b008bbf0fa4fe6d25d2e0ff7d2f10f
Sey Forasteira <75854840+SeyMotor@users.noreply.github.com>
2025-05-13 21:50:08 -0300

    Criação de esqueletos basicos para as classes
    Criei algumas das classes para passar dados do frontend para o backend

---
+++ data/Quiz.java
@@ -0,0 +1,68 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Quiz {
+    private final String quizID;
+    private final LocalDateTime quizCreationDate;
+    private final String quizCreatorID;
+    private String quizName;
+    private List<String> questionsIDList = new ArrayList<>();
+    private List<String> userFavoriteIDList = new ArrayList<>();
+    
+    public Quiz(String quizName, String quizCreatorID) {
+        this.quizID = UUID.randomUUID().toString();
+        this.quizCreationDate = LocalDateTime.now();
+        this.quizName = quizName;
+        this.quizCreatorID = quizCreatorID;
+    }
+    
+    public String getQuizID() {
+        return quizID;
+    }
+
+    public LocalDateTime getQuizCreationDate() {
+        return quizCreationDate;
+    }    
+    
+    public String getQuizCreatorID() {
+        return quizCreatorID;
+    }
+
+    public String getQuizName() {
+        return quizName;
+    }
+
+    public void setQuizName(String quizName) {
+        this.quizName = quizName;
+    }
+
+    public List<String> getQuestionsIDList() {
+        return questionsIDList;
+    }
+    
+    public int getQuizLength(){
+        return questionsIDList.size();
+    }
+    
+    public void setQuestionsIDList(List<String> questionsIDList) {
+        this.questionsIDList = questionsIDList;
+    }
+    
+    public void setQuestionIDfromList(String oldID, String newID){
+        this.questionsIDList.set(questionsIDList.indexOf(oldID), newID);
+    }
+
+    public List<String> getUserFavoriteIDList() {
+        return userFavoriteIDList;
+    }
+    
+    public void setNewUserFav(String userID){
+        this.userFavoriteIDList.add(userID);        
+    }
+    
+    public void removeFromUserFav(String userID){
+        this.userFavoriteIDList.remove(userFavoriteIDList.indexOf(userID));
+    }
+}
---
+++ data/User.java
@@ -0,0 +1,75 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class User {
+    private final String userID;
+    private final LocalDateTime userSignUpDate;
+    private String username;
+    private String userEmail;
+    private String userPassword;
+    private List<String> favoriteQuizIDList = new ArrayList<>();
+    private boolean Admin;
+
+    public User(String username, String userEmail, String userPassword) {
+        this.userID = UUID.randomUUID().toString();
+        this.userSignUpDate = LocalDateTime.now();
+        this.username = username;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.Admin = false;
+    }
+    
+    public String getUserID() {
+        return userID;
+    }
+
+    public LocalDateTime getUserSignUpDate() {
+        return userSignUpDate;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+    
+    public List<String> getFavoriteQuizIDList() {
+        return favoriteQuizIDList;
+    }
+    
+    public void setNewFavQuiz(String quizID){
+        this.favoriteQuizIDList.add(quizID);        
+    }
+    
+    public void removeFromFavQuiz(String quizID){
+        this.favoriteQuizIDList.remove(favoriteQuizIDList.indexOf(quizID));
+    }
+    
+    public boolean isAdmin() {
+        return Admin;
+    }
+
+    public void setAdmin(boolean Admin) {
+        this.Admin = Admin;
+    }
+}
