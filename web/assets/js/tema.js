8d826d579e68d00cb650f032c9ca3874149eb24f
Normilando <86866114+Normilando@users.noreply.github.com>
2025-06-09 21:44:55 -0300

    Remoção do JavaScript
    Removendo o javascript que não foi utilizado.

--- web/assets/js/tema.js
+++
@@ -1,7 +0,0 @@
-var root = document.querySelector(':root');
-
-function myFunction_set() {
-    root.style.setProperty('--cor_primaria', 'grey');
-    root.style.setProperty('--cor_secudaria', 'red');
-    root.style.setProperty('--cor_fundo', 'black');
-}
